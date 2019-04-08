d3049fa48a7d78fdc5b97cdf65af80c246d13cb4
mhayrapetyan <manehayrapetyan123@gmail.com>
2019-04-09 01:18:10 +0400

    Added a method to merge to sorted arrays

---
+++ homeworks/Mane's homework/RV_MergeTwoSortedArrays.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class RV_MergeTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] array01 = {1,2,7,8};
+        int[] array02 = {2,5,6,7,9,10};
+
+        int[] array = new int[array01.length + array02.length];
+        int i=0, j=0, k=0;
+        while (i < array01.length && j < array01.length) {
+            if (array01[i] < array02[j]) {
+                array[k++] = array01[i++];
+            } else {
+                array[k++] = array02[j++];
+            }
+        }
+
+        while (i < array01.length) array[k++] = array01[i++];
+        while (j < array02.length) array[k++] = array02[j++];
+        System.out.println(Arrays.toString(array));
+    }
+}
+
